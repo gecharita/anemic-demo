d081fae35edb5a3cba22da543b674c34ba434cfb
gecharita <gecharita@gmail.com>
2020-05-07 20:46:10 +0300

    TaxNumber REST method added

--- src/main/java/com/gecharita/anemic/model/Person.java
+++ src/main/java/com/gecharita/anemic/model/Person.java
@@ -1,5 +1,7 @@
 package com.gecharita.anemic.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -7,6 +9,7 @@ import javax.persistence.Table;
 import java.io.Serializable;
 
 @Entity
+@Data
 public class Person implements Serializable {
     @Id
     @GeneratedValue
@@ -16,64 +19,4 @@ public class Person implements Serializable {
     private String address;
     private String country;
     private int age;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPhone() {
-        return phone;
-    }
-
-    public void setPhone(int phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", phone=" + phone +
-                ", address='" + address + '\'' +
-                ", country='" + country + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
